a9926d9ef8278aae0f3cc392c24982cbc39e6df3
sdzharkov <stephan.zharkov@gmail.com>
2017-04-09 13:39:29 -0700

    Job creation process completed

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ var redis = require('redis');
 var client = redis.createClient(); //creates a new client
 var kue = require('kue');
 var queue = kue.createQueue();
+var axios = require('axios');
+var validUrl = require('valid-url');
 // var jobQueue = require('./jobQueue');
 
 client.on('connect', function() {
@@ -15,10 +17,25 @@ client.on('error', function (err) {
     console.log("Error " + err);
 });
 
+queue.on( 'error', function( err ) {
+  console.log( 'Oops... ', err );
+});
+
 function createJob(myUrl) {
-	var job = queue.create('request', myUrl).priority('high').save( function(err){
-	   if( !err ) console.log("Your new id for the url " + myUrl + " is " + job.id);
+	var job = queue.create('request', myUrl).priority('high').removeOnComplete( true ).save( function(err){
+	   if( !err ) {
+	    	console.log("Your new id for the url " + myUrl + " is " + job.id);
+	   		client.hmset(job.id, 0, 'none', redis.print);
+		}
 	});
+}
+
+function processRequest(Job, done) {
+	axios.get(Job.data)
+	  .then(function(response) {
+	  	client.hmset(Job.id, 1, response.data, redis.print);
+    	done();
+	  });
 }
 
 
@@ -39,11 +56,16 @@ app.get('/status/:id', function (req, res){
 	res.send(req.params);
 })
 
-app.get('/create/:url', function (req, res){
-	if (req.params['url'] != ""){
-		createJob(req.params['url'])
+app.get('/create/:url', function (req, res) {
+	// if (req.params['url'] != ""){
+
+	if (validUrl.isUri("http://" + req.params['url'])) {
+		res.send('valid')
+		createJob("http://" + req.params['url'])
 	}
-	res.send(req.params['url']);
+	else{
+		console.log("Invalid parameter")
+	}
 })
 
 app.listen(app.get('port'), function(){
@@ -51,7 +73,7 @@ app.listen(app.get('port'), function(){
 });
 
 queue.process('request', 20, function(job, done){
-  // ...
+  processRequest(job, done);
 });
 
 
